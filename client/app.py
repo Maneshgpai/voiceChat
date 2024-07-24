375ffcb360431f1dd2e0a2e6fcec10b806d058a8
Maneshgpai <maneshgpai@gmail.com>
2024-07-25 00:45:27 +0530

    Removed Chat pg
    Because serving only API. Chat interface not going to be maintained

--- client/app.py
+++ client/app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Voice Chat", layout="wide", page_icon=":material/
 
 pg = st.navigation([
         # st.Page("webpages/pg_home.py", title="Home", icon=":material/home:"),
-        st.Page("webpages/pg_chat.py", title="Chat", icon=":material/voice_chat:"),
+        # st.Page("webpages/pg_chat.py", title="Chat", icon=":material/voice_chat:"),
         st.Page("webpages/pg_settings.py", title="Character Settings", icon=":material/tune:"),
         st.Page("webpages/pg_addvoice.py", title="Voice Settings", icon=":material/settings_voice:"),
     ], position="sidebar")
