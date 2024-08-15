f6afccc838c083587fe8ead418058931d1cf8581
Maneshgpai <maneshgpai@gmail.com>
2024-08-15 21:53:59 +0530

    Issue with voice reachout

--- cicd/firebase/duplicate_document.py
+++ cicd/firebase/duplicate_document.py
@@ -14,7 +14,7 @@ cred_prod = credentials.Certificate("./cred_cert/prod_serviceAccountKey.json")
 firebase_admin.initialize_app(cred_prod, name='prod')
 db_prod = firestore.client(app=firebase_admin.get_app('prod'))
 
-def duplicate_document(collection_name, original_doc_id):
+def duplicate_document_to_same_collection(collection_name, original_doc_id):
     original_doc_ref = db_prod.collection(collection_name).document(original_doc_id)
     new_doc_ref = db_prod.collection(collection_name).document()
 
@@ -32,6 +32,22 @@ def duplicate_document(collection_name, original_doc_id):
     except Exception as e:
         print(f'Error duplicating document: {e}')
 
-duplicate_document("voiceClone_characters","jyNYGKS4xNjls2YbfFvV")
-duplicate_document("voiceClone_characters","kZ9OXDQLbU6MFCYXkIHm")
-duplicate_document("voiceClone_characters","uPJUM2kXjdnpkphuCPeO")
+def copy_document_from_prod_to_dev(prod_collection_name, original_doc_id, dev_collection_name):
+    original_doc_ref = db_prod.collection(prod_collection_name).document(original_doc_id)
+    new_doc_ref = db_dev.collection(dev_collection_name).document(original_doc_id)
+
+    try:
+        doc_snapshot = original_doc_ref.get()
+        
+        if not doc_snapshot.exists:
+            print(f'Original document {original_doc_id} does not exist!')
+            return
+        
+        original_data = doc_snapshot.to_dict()
+        new_doc_ref.set(original_data)
+        print(f'Document duplicated successfully: {new_doc_ref.id}')
+        
+    except Exception as e:
+        print(f'Error duplicating document: {e}')
+
+copy_document_from_prod_to_dev('voiceClone_characters', 'IJfd3UlnIo6vtbreVaQT', 'voiceClone_characters')
